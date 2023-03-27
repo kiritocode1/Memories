d39f0f9f080177dc70922f4abed8c914be40a816
kiritocode1 <kathawalearyan9@gmail.com>
2023-03-27 20:27:37 +0530

    Dev done

--- src/Components/MainPart.jsx
+++ src/Components/MainPart.jsx
@@ -1,85 +1,97 @@
 import React from "react";
 import { Textarea } from "@nextui-org/react";
-import { useState, UseEffect } from "react";
+import { useState} from "react";
 
-const InputArea = ({ state, function_call }) => {
-  const [Input, setInput] = useState("");
-  async function onSubmit(event) {
-    event.preventDefault();
-    try {
-      const response = await fetch("/api/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ request: Input }),
-      });
 
-      const data = await response.json();
-      if (response.status !== 200) {
-        throw (
-          data.error ||
-          new Error(`Request failed with status ${response.status}`)
-        );
-      }
-
-      function_call(data.result);
-      setInput("");
-    } catch (error) {
-      // Consider implementing your own error handling logic here
-      console.error(error);
-      alert(error.message);
-    }
-  }
-
-  return (
-    <div>
-      <Textarea
-        labelPlaceholder="
-        
-        im here to listen to you "
-        color="secondary"
-        status="secondary"
-        animated
-        required
-        shadow
-        minrows={10}
-        fullWidth
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            onSubmit(e);
-          }
-        }}
-      />
-    </div>
-  );
-};
-
-const OutputArea = () => {
-  return (
-    <span>
-      <Textarea
-        readOnly
-        status="success"
-        initialValue="loading... "
-        width="400px"
-        label="Responce "
-      />
-    </span>
-  );
-};
 
 const MainPart = () => {
+
+  const [Input, setInput] = useState("");
+	const [Result, setResult] = useState("");
+
+  async function onSubmit(event) {
+		event.preventDefault();
+
+		try {
+			const response = await fetch("/api/generate", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ request: Input }),
+			});
+			//console.log("Req sent !!!");
+			const data = await response.json();
+			if (response.status !== 200) {
+				throw (
+					data.error ||
+					new Error(`Request failed with status ${response.status}`)
+				);
+			}
+
+			setResult(data.result);
+			//console.log(`Data set ! ${data.result}`);
+			setInput("");
+		} catch (error) {
+			// Consider implementing your own error handling logic here
+			console.error(error);
+			alert(error.message);
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="flex flex-wrap justify-evenly  items-center w-full ">
+		<div className="flex flex-wrap justify-evenly  items-center w-full ">
       <div className="w-60">
-        <InputArea />
-      </div>
-      <div>
-        <OutputArea />
-      </div>
-    </div>
-  );
+        {/* Input area */}
+				<Textarea
+					labelPlaceholder="
+        im here to listen to you "
+					color="secondary"
+					status="secondary"
+					animated
+					required
+					shadow
+					minrows={10}
+					fullWidth
+					onKeyDown={(e) => {
+						if (e.key === "Enter") {
+							onSubmit(e);
+						}
+					}}
+					onChange={(e) => {
+						setInput(e.target.value);
+						//console.log(Input);
+					}}
+					value={Input}
+				/>
+			</div>
+			<div>
+        <span>
+          
+          {/* Output area */}
+					<Textarea
+						readOnly
+						status="success"
+						initialValue={`${Result}`}
+					  width="400px"
+					  hidden={Result===""?true:false}
+						label="Memory says "
+					/>
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default MainPart;
--- src/pages/api/generate.js
+++ src/pages/api/generate.js
@@ -16,22 +16,25 @@ export default async function gg(req, res) {
     return;
   }
 
-  const animal = req.body.animal || "";
-  if (animal.trim().length === 0) {
+  const Request = req.body.request || "";
+  if (Request.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid responce , thank you ",
       },
     });
     return;
   }
 
   try {
+
     const completion = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+      prompt: generatePrompt(Request),
+      temperature: 0.9,
+      max_tokens:3000, 
     });
+
     res.status(200).json({ result: completion.data.choices[0].text });
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
@@ -49,15 +52,25 @@ export default async function gg(req, res) {
   }
 }
 
-function generatePrompt(animal) {
-  const capitalizedAnimal = animal[0].toUpperCase() +
-    animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+function generatePrompt(Result) {
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
-Names:`;
+
+  return `pretend you are a therapist , and users are trying to seek help , 
+    i want you to comfort them , they are suffering from a mental illness , identify their problem , and  give them solutions so they can counter the problems , be kind and loving  , the past conversations are (read them carefully and analyse the problems , please try to have context of the conversation)
+    and the 
+    current convo is ${Result}, i want you to talk through all the problems , 
+    in a meaningful human way , and give them solutions so they can counter the problems , be kind and loving,
+    
+    give a pep talk  , in a more human way , your goal is to comfort the user , 
+    make it personal to you , tell them you care about them like a real person , give me a solution in 10-20 words  , not more and be more   kind   and understanding ,, 
+    the response should be in a human way , not a robot way , like a chat bot , for  eg " hey , i see this is your case ... " or "okay , listen im here , i didnt know you were suffering from ... " , 
+
+    be like a sage and do give words of comfort and  words of wisdom at the end 
+
+    also give me a medical solution , if applicable and safe to consume . 
+
+    based on the history and convo , i want you to tell me the problems i have or might possibly develop explicitly spell it out , eg " i think you might have ADHD " , 
+
+    dont just suggest  to go to a therapist , i want you to give me a solution ,
+    i cant really talk with another person about this`;
 }
